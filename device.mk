be2d71db8eaae7607be01eee4313dffd1b9bd1bb
karthik <karthik1896@outlook.com>
2020-09-30 17:56:49 +0530

    Lets nuke telephone-ext bootjar for now

--- device.mk
+++ device.mk
@@ -356,8 +356,8 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+#PRODUCT_BOOT_JARS += \
+    #telephony-ext
     
 # TetherOffload
 PRODUCT_PACKAGES += \
