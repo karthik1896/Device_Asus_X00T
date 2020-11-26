6f06f2341d64356dba677a3d780cc1dad93e0a82
karthik1896 <karthik1896@outlook.com>
2020-11-26 14:00:44 +0530

    Revert "Adapt to Syberia"
    This reverts commit 9e9d6e2455d6a91798ce531a431996a3556da976.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/syberia_X00T.mk
+    $(LOCAL_DIR)/lineage_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    syberia_X00T-user \
-    syberia_X00T-userdebug \
-    syberia_X00T-eng
+    lineage_X00T-user \
+    lineage_X00T-userdebug \
+    lineage_X00T-eng
