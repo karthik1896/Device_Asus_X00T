62390f582ad9304c2d4b23da64ea00f022f1fc9c
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-01-08 11:09:46 +0530

    X00T: Adapt to VoltageOS
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- voltage_X00T.mk
+++ voltage_X00T.mk
@@ -19,22 +19,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common VoltageOS stuff
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := X00T
 PRODUCT_MANUFACTURER := asus
-PRODUCT_NAME := lineage_X00T
-PRODUCT_MODEL := ASUS_X00T
-
-PRODUCT_GMS_CLIENTID_BASE := android-asus
+PRODUCT_MODEL := Asus Zenfone Max Pro M1
+PRODUCT_NAME := voltage_X00T
 TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+# VoltageOS Properties
+TARGET_BOOT_ANIMATION_RES := 2160
+VOLTAGE_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=X00T \
+    PRODUCT_DEVICE=X00T \
+    PRODUCT_NAME=WW_X00T \
+    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
