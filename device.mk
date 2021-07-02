2938c1690a2b586ab3894c0f2c92ca8402e14e45
susandahal <susandahal53@gmail.com>
2021-07-02 02:16:41 +0000

    ANT+ : Adapt to CAF

--- device.mk
+++ device.mk
@@ -17,10 +17,8 @@
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    antradio_app \
+    libantradio
 
 # Audio
 PRODUCT_PACKAGES += \
