3af083b4f659097a2f5bc248c1b623ed976cedcc
Ratoriku <a1063021545@gmail.com>
2021-10-10 12:07:09 +0530

    X00T:Build HIDL interface libs
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- device.mk
+++ device.mk
@@ -254,6 +254,22 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor \
     android.hidl.base@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
+    android.hardware.bluetooth@1.0.vendor \
+    android.frameworks.displayservice@1.0.vendor \
+    android.hardware.drm@1.4.vendor \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.media.c2@1.1.vendor \
+    android.hardware.neuralnetworks@1.3.vendor \
+    android.hardware.keymaster@4.0.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio@1.4.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.0.vendor \
+    android.system.net.netd@1.1.vendor \
+    android.hardware.gnss@2.1.vendor
+
 # HW Crypto
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
