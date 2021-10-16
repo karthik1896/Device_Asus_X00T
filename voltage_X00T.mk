e4234643cf811581f8ab93eceaf9f03ad0cf5ee2
Dmitrii <bankersenator@gmail.com>
2021-10-16 21:57:24 +0530

    X00T: Add Maintainer Overlay

--- voltage_X00T.mk
+++ voltage_X00T.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common Carbon stuff
 $(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
+# Official-ify
+VOLTAGE_BUILD_TYPE := OFFICIAL
+
 # Inherit from X00T device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
