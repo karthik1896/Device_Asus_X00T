0409c67fd7cec7ecb6d92e4552d4a91cb472dcd5
ElectroPerf <kunmun.devroms@gmail.com>
2022-06-26 06:09:02 +0000

    X00T: Build Camera
    - Camera Hal
    - Drop Snap As It Glitches In OSS Hal

--- device.mk
+++ device.mk
@@ -109,6 +109,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.sdm660
+    
+PRODUCT_PACKAGES += \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
